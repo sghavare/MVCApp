7cbca1938c271a5b2925ded06df8db3c47736363
Sachin Ghavare <Sachin.Ghavare@ey.com>
2019-06-22 18:30:44 -0400

    Add project files.

---
+++ eTFV/Data/ClientContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTFV.Models
+{
+    public class ClientContext : DbContext
+    {
+        public ClientContext (DbContextOptions<ClientContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<eTFV.Models.Client> Client { get; set; }
+    }
+}
---
+++ eTFV/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTFV.Models
+{
+    public class Client
+    {
+        public int ClientID { get; set; }
+
+        public string ClientName { get; set; }
+    }
+}
